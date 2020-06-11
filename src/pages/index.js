fa1f64a3dcf8ceac8d1ce9e76d036f8835ea81a6
keddisa <keddisa@gmail.com>
2020-06-11 16:15:44 -0400

    add customizable title card

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,10 +8,14 @@ import Journey from "../components/journey"
 import Portfolio from "../components/portfolio"
 import Testimonials from "../components/testimonials"
 import Contact from "../components/contact"
+import TitleCard from "../components/title-card"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <TitleCard  title="Andrew Keddis"
+                subTitle="Make ideas happen"
+                align="left"/>
     <Cover />
     <Skills />
     <Journey />
