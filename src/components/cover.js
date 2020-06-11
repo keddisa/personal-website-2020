cb3d54830d5067200298cab6b5c31561ff3ce7fb
keddisa <keddisa@gmail.com>
2020-06-10 22:43:43 -0400

    add cover section

--- src/components/cover.js
+++ src/components/cover.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+// import BackgroundImage from '../images/cover-image.jpg';
+// import { css } from 'emotion'
+
 const Cover = () => {
-    return (<div className="cover">
-        <div className="cover-title">
-            Cover Section
-            <div className="cover-subtitle">Slogan goes here</div>
-        </div>
+    return (<div className="cover" >
+        <div className="cover-title">Andrew Keddis</div>
+        <div className="cover-subtitle">Bring ideas to reality</div>
+        <div className="cover-subtitle2">Software Developer | Business Professional | Adventurer</div>
     </div>)
 }
 
