3d773b951ac75ff19b270a0923160524c4cc1dba
keddisa <keddisa@gmail.com>
2020-06-13 09:49:49 -0400

    add contact section and images to title cards

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,22 +8,34 @@ const IndexPage = () => (
     {/* <Cover /> */}
     <TitleCard  title="Andrew Keddis"
                 subTitle="Make ideas happen"
-                align="left"/>
+                align="left"
+                backgroundImage="skydiving"/>
     <TextContainer  sizeLarge={true}
                     content="Software Engineer | Educator | Business Professional | Adventurer | Traveler | Exercise Junkie"/>
     <TitleCard  title="My Playground"
                 subTitle="If the only tool you have is a hammer, it's hard to eat spaghetti"
-                align="right"/>
+                align="right"
+                backgroundImage="handyman"/>
     <Skills />
     <TitleCard  title="My Journey"
                 subTitle="How did I get here?"
-                align="center"/>
+                align="center"
+                backgroundImage="journey-title"/>
     <Journey />
     <TitleCard  title="Glance at my work"
                 subTitle="Check these samples out"
-                align="left"/>
+                align="left"
+                backgroundImage="portfolio-title"/>
     <Portfolio />
+    <TitleCard  title="Testimonials"
+                subTitle="What others have said"
+                align="right"
+                backgroundImage="testimonials-title"/>
     <Testimonials />
+    <TitleCard  title="Get in touch"
+                subTitle="Take action now"
+                align="center"
+                backgroundImage="contact-title"/>
     <Contact />
   </Layout>
 )
